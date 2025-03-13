2cacd721454cc4148c2dcdf7202795b7a4a0f762
Christina Hunter <frijole16@gmail.com>
2025-03-12 18:13:54 -0700

    Can now configure player names

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
 function openPlayerConfig(event) {
-  console.dir(event.target);
+  editedPlayer = +event.target.dataset.playerid; // the '+' symbol converts the string to a num
   playerConfigOverlayElement.style.display = "block";
   backdrop.style.display = "block";
 }
@@ -7,4 +7,28 @@ function openPlayerConfig(event) {
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = "none";
   backdrop.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
+  formElement.firstElementChild.lastElementChild.value = "";
+}
+
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayerName = formData.get("playername").trim(); //trims off excess whitespace before and after entry
+  if (!enteredPlayerName) {
+    //empty strings are considered falsey values
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "Please enter a valid name.";
+    return;
+  }
+
+  const updatedPlayerDataElement = document.getElementById(
+    "player-" + editedPlayer + "-data"
+  );
+  updatedPlayerDataElement.children[1].textContent = enteredPlayerName;
+
+  players[editedPlayer - 1].name = enteredPlayerName;
+
+  closePlayerConfig();
 }
